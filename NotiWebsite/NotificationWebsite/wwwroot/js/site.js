6c2f4b343a8474c548d3251c50e66f525047fba7
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-03-14 11:24:11 +0300

    Optimize js code for both forms
    Optimized functions to work with both forms(login, sign up). Added to the sign up window the same functional as login form had.

--- NotiWebsite/NotificationWebsite/wwwroot/js/site.js
+++ NotiWebsite/NotificationWebsite/wwwroot/js/site.js
@@ -1,41 +1,50 @@
 ﻿document.getElementById("login-win").style.display = "none";
 document.getElementById("sign-up-win").style.display = "none";
 
-function openLogin() {
-  const loginBtn = document.getElementById("loginBtn");
-  const modal = document.getElementById("login-win");
-  loginBtn.disabled = true;
-  modal.style.display = "block";
+function openAuthWin(modalWin, acceptBtn) {
+  modalWin.style.display = "block";
+  acceptBtn.disabled = true;
 }
-function closeLogin() {
-  const modal = document.getElementById("login-win");
-  const login_username = document.getElementById("login_username");
-  const login_password = document.getElementById("login_password");
-  const authBtnProp = document.querySelector(".auth-btn");
+function closeAuthWin(modal, inputsProp, authBtn) {
   modal.classList.add("modal-closing"); // Add the class to trigger the closing animation
   setTimeout(() => {
     modal.style.display = "none";
     modal.classList.remove("modal-closing");
-  }, 300); // since anim 0.3s we need 300ms
-  login_username.value = "";
-  login_password.value = "";
-  login_username.placeholder = "Username:";
-  login_password.placeholder = "Password";
-  authBtnProp.classList.remove("active");
-}
 
+    inputsProp.forEach((element) => {
+      element.value = "";
+      element.placeholder = `Enter ${element.getAttribute("name")}:`;
+    });
+
+    authBtn.classList.remove("active");
+  }, 300); // since anim 0.3s we need 300ms
+}
+function openLogin() {
+  const loginBtn = document.getElementById("loginBtn");
+  const modal = document.getElementById("login-win");
+  openAuthWin(modal, loginBtn);
+}
 function openSignup() {
   const modal = document.getElementById("sign-up-win");
   modal.style.display = "block";
 }
+
+function closeLogin() {
+  const modal = document.getElementById("login-win");
+  const login_username = document.getElementById("login_username");
+  const login_password = document.getElementById("login_password");
+  const authBtn = document.getElementById("loginBtn");
+  const loginProp = [login_username, login_password];
+  closeAuthWin(modal, loginProp, authBtn);
+}
 function closeSignUp() {
   const modal = document.getElementById("sign-up-win");
-  modal.classList.add("modal-closing"); //add closing class
-
-  setTimeout(() => {
-    modal.style.display = "none";
-    modal.classList.remove("modal-closing");
-  }, 300);
+  const mail = document.getElementById("signUp_mail");
+  const username = document.getElementById("signUp_username");
+  const password = document.getElementById("signUp_password");
+  registerProp = [mail, username, password];
+  const authBtnProp = document.getElementById("signUpBtn");
+  closeAuthWin(modal, registerProp, authBtnProp);
 }
 
 //view of username placeholder(Login):
@@ -47,7 +56,7 @@ login_username.addEventListener("blur", function () {
     login_username.setAttribute("placeholder", "Username:");
   }
 });
-//view if password placeholder(Login):
+//view of password placeholder(Login):
 login_password.addEventListener("focus", function () {
   login_password.removeAttribute("placeholder");
 });
@@ -56,21 +65,45 @@ login_password.addEventListener("blur", function () {
     login_password.setAttribute("placeholder", "Password:");
   }
 });
-//change auth btn
-function changeAuthBtn() {
-  const loginUsername = document.getElementById("login_username");
-  const loginPassword = document.getElementById("login_password");
-  const authBtnProp = document.querySelector(".auth-btn");
+
+//check that every input filled
+function checkFill(inputsProp) {
+  for (let element of inputsProp) {
+    if (element.value.trim() === "") {
+      return false;
+    }
+  }
+  return true;
+}
+//change the login btn state(active/disabled)
+function changeStateLoginBtn() {
   const loginBtn = document.getElementById("loginBtn");
-  if (loginUsername.value.trim() !== "" && loginPassword.value.trim() !== "") {
-    authBtnProp.classList.remove("disable");
-    authBtnProp.classList.add("active");
-    loginBtn.disabled = false;
-  } else if (authBtnProp.classList.contains("active")) {
-    authBtnProp.classList.add("disable");
-    authBtnProp.classList.remove("active");
-    loginBtn.disabled = true;
+  const username = document.getElementById("login_username");
+  const password = document.getElementById("login_password");
+  inputsProp = [username, password];
+  changeAuthBtn(inputsProp, loginBtn);
+}
+//change the sign up btn state(active/disabled)
+function changeStateSignUpBtn() {
+  const signUpBtn = document.getElementById("signUpBtn");
+  const mail = document.getElementById("signUp_mail");
+  const username = document.getElementById("signUp_username");
+  const password = document.getElementById("signUp_password");
+  inputsProp = [mail, username, password];
+  changeAuthBtn(inputsProp, signUpBtn);
+}
+
+//change auth btn
+function changeAuthBtn(inputsProp, btn) {
+  if (checkFill(inputsProp)) {
+    btn.classList.remove("disable");
+    btn.classList.add("active");
+    btn.disabled = false;
+  } else if (btn.classList.contains("active")) {
+    btn.classList.add("disable");
+    btn.classList.remove("active");
+    btn.disabled = true;
   } else {
-    loginBtn.disabled = true;
+    btn.disabled = true;
   }
 }
