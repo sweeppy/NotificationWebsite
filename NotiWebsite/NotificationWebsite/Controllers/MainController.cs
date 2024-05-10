863d3fdae039facc190ed90956d440823a27a27d
sweeppy <104524272+sweeppy@users.noreply.github.com>
2024-05-10 11:52:49 +0300

    Minor changes

--- NotiWebsite/NotificationWebsite/Controllers/MainController.cs
+++ NotiWebsite/NotificationWebsite/Controllers/MainController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using NotificationWebsite.DataAccess.Data;
 using NotificationWebsite.Models;
 using NotificationWebsite.Utility.Helpers.Jwt;
 
@@ -8,14 +7,11 @@ namespace NotificationWebsite.Controllers
     public class MainController : Controller
     {
         private readonly ILogger<MainController> _logger;
-        private readonly IUserRepository _user_rep;
 
         private readonly IJwtService _jwtService;
-        public MainController(ILogger<MainController> logger,
-         IUserRepository userRepository, IJwtService jwtService)
+        public MainController(ILogger<MainController> logger, IJwtService jwtService)
         {
             _logger = logger;
-            _user_rep = userRepository;
             _jwtService = jwtService;
         }
 
